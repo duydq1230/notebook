19a1058b2c83c7260f30469d9e985d38f5f50981
rain <brother.rain.1024@gmail.com>
2018-01-25 08:59:38 +0700

    rebuild

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+name=notebook_full
 
+rm -rf out/*.csv
 
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out -aux-directory=auxil notebook.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out -aux-directory=auxil $name.tex
 
-makeindex out/notebook.idx
+makeindex out/$name.idx
 
-bibtex out/notebook.aux
+bibtex out/$name.aux
 
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out -aux-directory=auxil notebook.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out -aux-directory=auxil $name.tex
 
 #pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=/home/rain/PycharmProjects/notes/out -aux-directory=/home/rain/PycharmProjects/notes/auxil note.tex
