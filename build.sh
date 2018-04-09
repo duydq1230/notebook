ac6a223099833166e2ec929d47b5fafc5a4a510c
Vu Anh <anhv.ict91@gmail.com>
2018-04-09 14:23:23 +0700

    update build script

--- build.sh
+++ build.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
 name=python
+
+echo $#
+
+if [ $# -eq 1 ]; then
+    name=$1
+fi
+
 declare -A folders
 folders=(
+  ["latex"]="books/computer_science/latex"
+  ["docker"]="books/computer_science/docker"
   ["python"]="books/programming/python"
 )
+folder=${folders[$name]}
 
 mkdir out
 
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out -aux-directory=auxil ${folders[$name]}/index.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out $folder/index.tex
 
 
 makeindex out/index.idx
-#
+
 bibtex out/index.aux
 
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out -aux-directory=auxil ${folders[$name]}/index.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out $folder/index.tex
 
-cp out/index.pdf books/programming/python/python.pdf
+cp out/index.pdf $folder/$name.pdf
+
 rm -rf out
+
+echo -e "\nBuild $name successfully"
